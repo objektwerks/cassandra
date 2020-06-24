58b98430bbc1f4caf80027c78f3f342f8c7f8c55
objektwerks <objektwerks@runbox.com>
2020-06-24 09:35:56 -0400

    refactored test

--- src/test/scala/cassandra/CassandraTest.scala
+++ src/test/scala/cassandra/CassandraTest.scala
@@ -1,38 +1,35 @@
 package cassandra
 
-import java.net.InetSocketAddress
-
 import com.datastax.oss.driver.api.core.CqlSession
 
-import org.scalatest.BeforeAndAfterAll
+import java.net.InetSocketAddress
+
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
-class CassandraTest extends AnyFunSuite with BeforeAndAfterAll with Matchers {
-  val address = new InetSocketAddress("127.0.0.1", 9042)
-  val session = CqlSession
-    .builder
-    .addContactPoint(address)
-    .build()
-
-  override protected def beforeAll(): Unit = {
+class CassandraTest extends AnyFunSuite with Matchers {
+  test("write > read > update > delete") {
+    val address = new InetSocketAddress("127.0.0.1", 9042)
+    val session = CqlSession
+      .builder()
+      .addContactPoint(address)
+      .build()
+    
     session.execute("DROP KEYSPACE IF EXISTS test;")
     session.execute("CREATE KEYSPACE test WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 1 };")
     session.execute("CREATE TABLE test.kv(key text, value int, PRIMARY KEY (key));")
-    ()
-  }
-
-  override protected def afterAll(): Unit = session.close()
-  
-  test("write > read > update > delete") {
+ 
     session.execute("INSERT INTO test.kv(key, value) VALUES ('k1', 1);")
     session.execute("INSERT INTO test.kv(key, value) VALUES ('k2', 2);")
     session.execute("INSERT INTO test.kv(key, value) VALUES ('k3', 3);")
+
     session.execute("select * from test.kv").all.size shouldBe 3
     session.execute("select value from test.kv where key = 'k3'").all.size shouldBe 1
     session.execute("update test.kv set value = 30 where key = 'k3'").all.size shouldBe 0
     session.execute("select value from test.kv where key = 'k3'").all.get(0).getInt("value") shouldBe 30
     session.execute("delete from test.kv where key = 'k3'").all.size shouldBe 0
     session.execute("select value from test.kv where key = 'k3'").all.size shouldBe 0
+
+    session.close()
   }
 }
