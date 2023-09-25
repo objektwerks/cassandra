672f21aee61061e8127bcef09e4fb470d5ea28c0
Objektwerks <objektwerks@runbox.com>
2023-09-25 11:38:09 -0400

    Update CassandraTest.scala

--- src/test/scala/cassandra/CassandraTest.scala
+++ src/test/scala/cassandra/CassandraTest.scala
@@ -7,8 +7,8 @@ import java.net.InetSocketAddress
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
-class CassandraTest extends AnyFunSuite with Matchers:
-  test("write > read > update > delete") {
+final class CassandraTest extends AnyFunSuite with Matchers:
+  test("write > read > update > delete"):
     val address = new InetSocketAddress("127.0.0.1", 9042)
     val datacenter = "datacenter1"
     val session = CqlSession
@@ -33,4 +33,3 @@ class CassandraTest extends AnyFunSuite with Matchers:
     session.execute("select value from test.kv where key = 'k3'").all.size shouldBe 0
 
     session.close()
-  }
